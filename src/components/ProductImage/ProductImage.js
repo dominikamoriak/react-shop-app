9c89ff427f1e5602d6b2759300dd6b48c16e310b
Dominika Moriak <dominika.moriak@gmail.com>
2023-04-26 23:25:29 +0200

    Edit

--- src/components/ProductImage/ProductImage.js
+++ src/components/ProductImage/ProductImage.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import styles from './ProductImage.module.scss';
 
 const ProductImage = ({ name, currentColor }) => {
- const imageUrl = `${process.env.PUBLIC_URL}/images/products/shirt-${title}--${currentColor}.jpg`;
+ const imageUrl = `${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`;
 
  return (
     <div className={styles.imageContainer}>
