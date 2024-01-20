5421b5181b9f635de748e9e7908c19e671704210
Dominika Moriak <dominika.moriak@gmail.com>
2024-01-20 16:42:57 +0100

    Add React.StrictMode to the index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+  <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
